430f4039f96850ba79db9974b0b1daf2e242b302
Lovisa Hallgren <lovisahallgren1@gmail.com>
2018-10-29 18:40:16 +0100

    Added text in footer

--- footer.php
+++ footer.php
@@ -5,4 +5,7 @@ declare(strict_types=1);
 ?>
 <script type="text/javascript" src="script.js"></script>
 </body>
+<footer>
+    <h5>Copyright &copy; Lovisa Hallgren, Yrgo 2018</h5>
+</footer>
 </html>

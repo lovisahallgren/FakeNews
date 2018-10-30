2a88dfaf61fedb6884d13b47a6c8a7afa95e2d94
Lovisa Hallgren <lovisahallgren1@gmail.com>
2018-10-30 12:55:57 +0100

    Changed date function since it didn't work

--- data.php
+++ data.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 require __DIR__.'/functions.php';
 
-
 $authors = [
   [
   'name' => 'Walter White',
@@ -32,70 +31,70 @@ $newsFeed = [
     'title' => 'No speeches. Short speech.',
     'content' => file_get_contents(__DIR__.'/content/article01.txt'),
     'author' => $authors[0]['name'],
-    'published date' => getDateFromTextfile($filename = __DIR__.'/content/article01.txt'),
+    'published date' => randomDate('January', 'December'),
     'like counter' => 4,
   ],
   [
     'title' => 'Four pounds... foooour pounds',
     'content' => file_get_contents(__DIR__.'/content/article02.txt'),
     'author' => $authors[0]['name'],
-    'published date' => getDateFromTextfile($filename = __DIR__.'/content/article02.txt'),
+    'published date' => randomDate('January', 'December'),
     'like counter' => 27,
   ],
   [
     'title' => 'Sorry, buddy.',
     'content' => file_get_contents(__DIR__.'/content/article03.txt'),
     'author' => $authors[1]['name'],
-    'published date' => getDateFromTextfile($filename = __DIR__.'/content/article03.txt'),
+    'published date' => randomDate('January', 'December'),
     'like counter' => 15,
   ],
   [
     'title' => 'Walter, you\'ve been busy.',
     'content' => file_get_contents(__DIR__.'/content/article04.txt'),
     'author' => $authors[1]['name'],
-    'published date' => getDateFromTextfile($filename = __DIR__.'/content/article04.txt'),
+    'published date' => randomDate('January', 'December'),
     'like counter' => 2,
   ],
   [
     'title' => 'What\'s your name?',
     'content' => file_get_contents(__DIR__.'/content/article05.txt'),
     'author' => $authors[2]['name'],
-    'published date' => getDateFromTextfile($filename = __DIR__.'/content/article05.txt'),
+    'published date' => randomDate('January', 'December'),
     'like counter' => 63,
   ],
   [
     'title' => 'Chemotherapy and marijuana go together like apple pie and Chevrolet.',
     'content' => file_get_contents(__DIR__.'/content/article06.txt'),
     'author' => $authors[2]['name'],
-    'published date' => getDateFromTextfile($filename = __DIR__.'/content/article06.txt'),
+    'published date' => randomDate('January', 'December'),
     'like counter' => 43,
   ],
   [
     'title' => 'A tiara... a white',
     'content' => file_get_contents(__DIR__.'/content/article07.txt'),
     'author' => $authors[3]['name'],
-    'published date' => getDateFromTextfile($filename = __DIR__.'/content/article07.txt'),
+    'published date' => randomDate('January', 'December'),
     'like counter' => 29,
   ],
   [
     'title' => 'Wayfarer 515, radio check.',
     'content' => file_get_contents(__DIR__.'/content/article08.txt'),
     'author' => $authors[3]['name'],
-    'published date' => getDateFromTextfile($filename = __DIR__.'/content/article08.txt'),
+    'published date' => randomDate('January', 'December'),
     'like counter' => 37,
   ],
   [
     'title' => 'You are done. Fired.',
     'content' => file_get_contents(__DIR__.'/content/article09.txt'),
     'author' => $authors[4]['name'],
-    'published date' => getDateFromTextfile($filename = __DIR__.'/content/article09.txt'),
+    'published date' => randomDate('January', 'December'),
     'like counter' => 39,
   ],
   [
     'title' => 'Ding ding ding ding.',
     'content' => file_get_contents(__DIR__.'/content/article10.txt'),
     'author' => $authors[4]['name'],
-    'published date' => getDateFromTextfile($filename = __DIR__.'/content/article10.txt'),
+    'published date' => randomDate('January', 'December'),
     'like counter' => 22,
   ],
 ];
--- functions.php
+++ functions.php
@@ -2,19 +2,27 @@
 
 declare(strict_types=1);
 
+// Stops time in PHP, helps me to get the random date to stay the same when refreshing the page
+srand(mktime(0, 0, 0));
+// $time = rand( 0, time() );
 
 /**
  * Get date from given textfile.
  *
- * @param  string $filename
+ * @param  string $start_date
+ * @param  string $end_date
  *
  * @return string
  */
-function getDateFromTextfile(string $filename): string
+ function randomDate(string $start_date, string $end_date): string
 {
-  if (file_exists($filename)) {
-    return date ("d F Y H:i:s", filemtime($filename));
-  }
+    // Convert to timetamps
+    $min = strtotime($start_date);
+    $max = strtotime($end_date);
+     // Generate random number using above bounds
+    $val = rand($min, $max);
+     // Convert back to desired date format
+    return date('d F Y', $val);
 }
 
 /**
